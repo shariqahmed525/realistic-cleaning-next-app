bdae57b82e13961b56f960db45b5a4d42052ebf4
shariqahmed525 <shariq.ahmed525@gmail.com>
2021-06-26 21:26:48 +0500

    added facebook pixel code

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,53 @@ import {
     ServicesIncludes,
 } from "../components";
 const Home = () => {
+    `
+    
+    
+    `;
+
     return (
         <>
             <Head>
                 <title>Realistic Cleaning</title>
                 <link rel="icon" href="/favicon.ico" />
+                <script>
+                    {(() => {
+                        !(function (f, b, e, v, n, t, s) {
+                            if (f.fbq) return;
+                            n = f.fbq = function () {
+                                n.callMethod
+                                    ? n.callMethod.apply(n, arguments)
+                                    : n.queue.push(arguments);
+                            };
+                            if (!f._fbq) f._fbq = n;
+                            n.push = n;
+                            n.loaded = !0;
+                            n.version = "2.0";
+                            n.queue = [];
+                            t = b.createElement(e);
+                            t.async = !0;
+                            t.src = v;
+                            s = b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t, s);
+                        })(
+                            window,
+                            document,
+                            "script",
+                            "https://connect.facebook.net/en_US/fbevents.js"
+                        );
+                        fbq("init", "339113737618805");
+                        fbq("track", "PageView");
+                    })()}
+                </script>
+                <noscript>
+                    <img
+                        width="1"
+                        height="1"
+                        style="display:none"
+                        src="https://www.facebook.com/tr?id=339113737618805&ev=PageView&noscript=1"
+                    />
+                </noscript>
             </Head>
             <Navbar />
             <Banner />
