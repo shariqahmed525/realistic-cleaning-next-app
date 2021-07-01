a01d1cca3824ab05d5d69b1e64229cad3a873789
shariqahmed525 <shariq.ahmed525@gmail.com>
2021-07-02 04:39:15 +0500

    update email sender

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 const emailSender = async (obj) => {
     try {
-        await axios.post(
+        const { data } = await axios.post(
             "https://realistic-cleaning-send-mail.herokuapp.com/",
             { ...obj }
         );
+        console.log(data);
     } catch (error) {
         console.log("got error in email sender ===> ", error.message);
     }
