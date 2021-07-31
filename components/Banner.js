c7bde3b55fff15ec51345ef1d70ef98f6f85bd51
shariqahmed525 <shariq.ahmed525@gmail.com>
2021-07-31 17:37:32 +0500

    update image size

--- components/Banner.js
+++ components/Banner.js
@@ -95,17 +95,17 @@ const Banner = () => {
                 >
                     <img
                         src="/images/cell.png"
-                        className="w-4 sm:w-6 object-contain inline mr-2 sm:mr-4"
+                        className="w-6 object-contain inline mr-3 sm:mr-4"
                     />
                     289-812-3444
                 </a>
                 <a
-                    className="font-bold text-2xl text-center lg:text-left text-white mt-3"
+                    className="font-bold text-lg sm:text-2xl text-center lg:text-left text-white mt-3"
                     href="mailto:realisticcleaning1@gmail.com"
                 >
                     <img
                         src="/images/email.png"
-                        className="w-5 sm:w-7 object-contain inline mr-2 sm:mr-4"
+                        className="w-4 sm:w-7 object-contain inline mr-3 sm:mr-4"
                     />
                     realisticcleaning1@gmail.com
                 </a>
