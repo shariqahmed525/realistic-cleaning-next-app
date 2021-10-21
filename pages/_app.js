0a20837c6d1b0025f754ae624e0b323196097904
Shariq Ahmed <shariq.ahmed525@gmail.com>
2021-10-22 04:02:28 +0500

    update code

--- pages/_app.js
+++ pages/_app.js
@@ -9,26 +9,22 @@ const App = ({ Component, pageProps }) => {
   const router = useRouter();
 
   useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init(FB_PIXEL_ID);
-        ReactPixel.pageView();
-
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      })
-      .catch((e) => {
-        console.log("got error in facebook pixel ===> ", e);
-      });
-
+    // import("react-facebook-pixel")
+    //   .then((x) => x.default)
+    //   .then((ReactPixel) => {
+    //     ReactPixel.init(FB_PIXEL_ID);
+    //     ReactPixel.pageView();
+    //     router.events.on("routeChangeComplete", () => {
+    //       ReactPixel.pageView();
+    //     });
+    //   })
+    //   .catch((e) => {
+    //     console.log("got error in facebook pixel ===> ", e);
+    //   });
     // const handleRouteChange = (url) => {
     //     gtag.pageview(url);
     // };
-
     // router.events.on("routeChangeComplete", handleRouteChange);
-
     // return () => {
     //     router.events.off("routeChangeComplete", handleRouteChange);
     // };
